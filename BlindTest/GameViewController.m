2cb9c6edbb112d66276b339ed33d41e1ff9663d4
Adrien BRAULT <monsti@gmail.com>
2011-05-23 19:44:22 +0200

    Removed tips

--- BlindTest/GameViewController.m
+++ BlindTest/GameViewController.m
@@ -21,40 +21,6 @@
 {
     if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
         self.navigationItem.title = @"Blind Test";
-        
-        /*
-        // Recuperer les musiques.
-        NSArray *musics = [Music loadMusicsFromBundle];
-        
-        // Recuperer la dernière musique du tableau.
-        Music *lastMusic = [musics lastObject];
-        
-        // Demarer la lecture de la musique.
-        [[MusicPlayer sharedPlayer] playMusic:lastMusic];
-        
-//        MusicPlayer *player = [MusicPlayer sharedPlayer];
-//        [player pause];
-//        [player play];
-//        [player stop];
-        
-        // Recuperer une copie mélangée du tableau des musiques.
-        NSArray *musicsShuffled = [musics shuffledCopy];
-        
-        // Generer un nombre aléatoire inclu dans [1-100]
-        int randomNumber = arc4random() % 100 + 1;
-        
-        // Generer un index aléatoire.
-        int randomMusicIndex = arc4random() % [musicsShuffled count];
-        
-        // Recuperer une musique d'un tableau.
-        Music *aMusic = [musicsShuffled objectAtIndex:randomMusicIndex];
-        
-        // Afficher le titre, l'artiste et la durée d'une musique dans la console.
-        NSLog(@"Titre: %@ - Artiste: %@ - Durée: %f s", aMusic.title, aMusic.artist, aMusic.duration);
-        
-        
-        [musicsShuffled release];
-        */
     }
     return self;
 }
